fedeaf9afbfa9b2c6e176aa5112a420b4ae3a83d
Jorgen Thelin <jthelin@microsoft.com>
2015-11-11 15:09:27 -0800

    Add meta-build script to build and assembly all the runtime artifacts.
    - Add meta-build script to run builds for the spark and csharp components, and then assembly the runtime artifacts into required SPARKCLR_HOME sub-directory structure.

---
+++ Build.cmd
@@ -0,0 +1,39 @@
+@setlocal
+@echo OFF
+
+SET CMDHOME=%~dp0
+@REM Remove trailing backslash \
+set CMDHOME=%CMDHOME:~0,-1%
+
+set SPARKCLR_HOME=%CMDHOME%\run
+@echo SPARKCLR_HOME=%SPARKCLR_HOME%
+
+if NOT EXIST "%SPARKCLR_HOME%" mkdir "%SPARKCLR_HOME%"
+if NOT EXIST "%SPARKCLR_HOME%\bin" mkdir "%SPARKCLR_HOME%\bin"
+if NOT EXIST "%SPARKCLR_HOME%\data" mkdir "%SPARKCLR_HOME%\data"
+if NOT EXIST "%SPARKCLR_HOME%\lib" mkdir "%SPARKCLR_HOME%\lib"
+if NOT EXIST "%SPARKCLR_HOME%\samples" mkdir "%SPARKCLR_HOME%\samples"
+if NOT EXIST "%SPARKCLR_HOME%\scripts" mkdir "%SPARKCLR_HOME%\scripts"
+
+@echo Assemble SparkCLR Scala components
+pushd "%CMDHOME%\scala"
+call mvn.cmd package
+@echo SparkCLR Scala binaries
+copy /y target\*.jar "%SPARKCLR_HOME%\lib\"
+popd
+
+@echo Assemble SparkCLR C# components
+pushd "%CMDHOME%\csharp"
+call Build.cmd
+@echo SparkCLR C# binaries
+copy /y Worker\Microsoft.Spark.CSharp\bin\Release\* "%SPARKCLR_HOME%\bin\"
+@echo SparkCLR C# Samples binaries
+copy /y Samples\Microsoft.Spark.CSharp\bin\Release\* "%SPARKCLR_HOME%\samples\"
+@echo SparkCLR Samples data
+copy /y Samples\Microsoft.Spark.CSharp\data\* "%SPARKCLR_HOME%\data\"
+popd
+
+@echo Assemble SparkCLR script components
+pushd "%CMDHOME%\scripts"
+copy /y *.cmd  "%SPARKCLR_HOME%\scripts\"
+popd
