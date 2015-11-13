76f41453ef350f4aa20d359bb89a87805cd210c2
tqin <tqin@microsoft.com>
2015-11-12 18:28:17 -0800

    Add Unit Test case for DataFrame Collect() API

--- csharp/AdapterTest/DataFrameTest.cs
+++ csharp/AdapterTest/DataFrameTest.cs
@@ -2,9 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+
 using AdapterTest.Mocks;
 using Microsoft.Spark.CSharp.Core;
 using Microsoft.Spark.CSharp.Sql;
+using Microsoft.Spark.CSharp.Proxy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdapterTest
@@ -29,7 +32,95 @@ namespace AdapterTest
             Assert.AreEqual(@"c:\path\to\input2.json", paramValuesToSecondDataFrameJsonFileMethod[0]);
             Assert.AreEqual("JoinCol", paramValuesToJoinMethod[1]);
         }
-    }
 
+        [TestMethod]
+        public void TestDataFrameCollect()
+        {
+            string jsonSchema = @"
+                {
+                  ""type"" : ""struct"",
+                  ""fields"" : [ {
+                    ""name"" : ""address"",
+                    ""type"" : {
+                      ""type"" : ""struct"",
+                      ""fields"" : [ {
+                        ""name"" : ""city"",
+                        ""type"" : ""string"",
+                        ""nullable"" : true,
+                        ""metadata"" : { }
+                      }, {
+                        ""name"" : ""state"",
+                        ""type"" : ""string"",
+                        ""nullable"" : true,
+                        ""metadata"" : { }
+                      } ]
+                    },
+                    ""nullable"" : true,
+                    ""metadata"" : { }
+                  }, {
+                    ""name"" : ""age"",
+                    ""type"" : ""long"",
+                    ""nullable"" : true,
+                    ""metadata"" : { }
+                  }, {
+                    ""name"" : ""id"",
+                    ""type"" : ""string"",
+                    ""nullable"" : true,
+                    ""metadata"" : { }
+                  }, {
+                    ""name"" : ""name"",
+                    ""type"" : ""string"",
+                    ""nullable"" : true,
+                    ""metadata"" : { }
+                  } ]
+                }";
+
+            int localPort = 4000;
+            object row1 = new object[] {
+                new object[] {"Columbus", "Ohio"},
+                34,
+                "123",
+                "Bill"
+            };
+
+            object row2 = new object[] {
+                new object[] {"Seattle", "Washington"},
+                43,
+                "789",
+                "Bill"
+            };
+
+            IStructTypeProxy structTypeProxy = new MockStructTypeProxy(jsonSchema);
+            IDataFrameProxy dataFrameProxy =
+                new MockDataFrameProxy(localPort,
+                                       new List<object>() { row1, row2 },
+                                       structTypeProxy);
+            DataFrame dataFrame = new DataFrame(dataFrameProxy, null);
+
+            List<Row> rows = new List<Row>();
+            foreach (var row in dataFrame.Collect())
+            {
+                rows.Add(row);
+                Console.WriteLine("{0}", row);
+            }
+
+            Assert.AreEqual(rows.Count, 2);
+            Row firstRow = rows[0];
+
+            string id = firstRow.GetAs<string>("id");
+            Assert.IsTrue(id.Equals("123"));
+            string name = firstRow.GetAs<string>("name");
+            Assert.IsTrue(name.Equals("Bill"));
+            int age = firstRow.GetAs<int>("age");
+            Assert.AreEqual(age, 34);
+
+            Row address = firstRow.GetAs<Row>("address");
+            Assert.AreNotEqual(address, null);
+            string city = address.GetAs<string>("city");
+            Assert.IsTrue(city.Equals("Columbus"));
+            string state = address.GetAs<string>("state");
+            Assert.IsTrue(state.Equals("Ohio"));
+        }
+    }
 
 }
---
+++ csharp/AdapterTest/Mocks/MockStructTypeProxy.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Spark.CSharp.Proxy;
+
+namespace AdapterTest.Mocks
+{
+    class MockStructTypeProxy : IStructTypeProxy
+    {
+        private string mockJson;
+
+        internal MockStructTypeProxy(string json)
+        {
+            mockJson = json;
+        }
+
+        public List<IStructFieldProxy> GetStructTypeFields()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToJson()
+        {
+            return mockJson;
+        }
+    }
+}
