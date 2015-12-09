207b892f21ff02111bd4a0deed7f90534909e1cd
skaarthik <skaarthik@gmail.com>
2015-12-08 16:03:58 -0800

    adding scala perf suite

---
+++ scala/perf/src/main/com/microsoft/spark/csharp/PerfSuite.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.microsoft.spark.csharp;
+
+/**
+ * Annotation used for marking a perf suite
+ */
+public @interface PerfSuite {}
