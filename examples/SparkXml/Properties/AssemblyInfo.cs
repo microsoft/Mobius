521b4ad57716f437bf866512a38a913b2b8b6ed7
skaarthik <skaarthik@gmail.com>
2016-01-31 21:08:43 -0800

    spark-xml example and updated SparkCLR version references

--- examples/SparkXml/Properties/AssemblyInfo.cs
+++ examples/SparkXml/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SparkCLR-Pi")]
-[assembly: AssemblyDescription("SparkCLR Pi example.")]
+[assembly: AssemblyTitle("SparkCLRXmlExample")]
+[assembly: AssemblyDescription("XML processing Example for DataFrame API in SparkCLR")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("Microsoft SparkCLR")]
