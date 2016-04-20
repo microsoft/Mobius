bb25971dead331f5de84009c8c30175d7298b0d0
skaarthik <skaarthik@gmail.com>
2016-04-20 14:33:33 -0700

    adding example to process Kafka messages using Spark Streaming C# app and other minor updates

--- examples/Sql/JdbcDataFrame/Properties/AssemblyInfo.cs
+++ examples/Sql/JdbcDataFrame/Properties/AssemblyInfo.cs
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SparkCLRJdbcDataFrameExample")]
-[assembly: AssemblyDescription("JDBC Example for DataFrame API in SparkCLR")]
+[assembly: AssemblyDescription("JDBC Example for DataFrame API in Mobius")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft SparkCLR")]
+[assembly: AssemblyProduct("Microsoft Mobius")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
