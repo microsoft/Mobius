59a35c4d05519138455fa538665e709df407c10d
tawan0109 <tawan@microsoft.com>
2016-05-17 21:14:48 +0800

    Use Apache commons compress library to zip&unzip Mobius application

--- scala/src/main/org/apache/spark/util/csharp/Utils.scala
+++ scala/src/main/org/apache/spark/util/csharp/Utils.scala
@@ -5,20 +5,41 @@
 package org.apache.spark.util.csharp
 
 import java.io._
-import java.util.{TimerTask, Timer}
-import java.util.zip.{ZipEntry, ZipOutputStream, ZipFile}
+import java.util.{Timer, TimerTask}
+import scala.collection.JavaConverters._
+import java.nio.file._
+import java.nio.file.attribute.PosixFilePermission
+
+import org.apache.commons.compress.archivers.zip.ZipFile
+import org.apache.commons.compress.archivers.zip.{ZipArchiveEntry, ZipArchiveOutputStream}
 import org.apache.commons.io.IOUtils
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Set
 
 /**
  * Utility methods used by SparkCLR.
  */
 object Utils {
 
+  val isPosix = FileSystems.getDefault().supportedFileAttributeViews().contains("posix")
+  val posixPermissionToInteger: Map[PosixFilePermission, Integer] = Map(
+    PosixFilePermission.OWNER_EXECUTE -> 0100,
+    PosixFilePermission.OWNER_WRITE -> 0200,
+    PosixFilePermission.OWNER_READ -> 0400,
+
+    PosixFilePermission.GROUP_EXECUTE -> 0010,
+    PosixFilePermission.GROUP_WRITE -> 0020,
+    PosixFilePermission.GROUP_READ -> 0040,
+
+    PosixFilePermission.OTHERS_EXECUTE -> 0001,
+    PosixFilePermission.OTHERS_WRITE -> 0002,
+    PosixFilePermission.OTHERS_READ ->0004)
+
   /**
    * List all entries of a zip file
-   * @param file zip file
+    *
+    * @param file zip file
    */
   def listZipFileEntries(file: File): Seq[String] = {
 
@@ -26,7 +47,7 @@ object Utils {
     val zipFile = new ZipFile(file)
 
     try {
-      val entries = zipFile.entries()
+      val entries = zipFile.getEntries
       while (entries.hasMoreElements()) {
         result.append(entries.nextElement().getName)
       }
@@ -39,6 +60,7 @@ object Utils {
 
   /**
    * Compress all files under given directory into one zip file and drop it to the target directory
+   *
    * @param sourceDir the directory where the zip file will be created
    * @param targetZipFile
    */
@@ -46,12 +68,11 @@ object Utils {
     if (!sourceDir.exists() || !sourceDir.isDirectory) {
       return
     }
-
     var fos: FileOutputStream = null
-    var zos: ZipOutputStream = null
+    var zos: ZipArchiveOutputStream = null
     try {
       fos = new FileOutputStream(targetZipFile)
-      zos = new ZipOutputStream(fos)
+      zos = new ZipArchiveOutputStream(fos)
       zipDir(sourceDir, sourceDir, zos)
     } catch {
       case e: Exception => throw e
@@ -61,17 +82,41 @@ object Utils {
     }
   }
 
-  private def zipDir(rootDir: File, sourceDir: File, out: ZipOutputStream): Unit = {
+  private def getUnixPermissionCode(permissionSet: Set[PosixFilePermission]): Integer = {
+    var number = 0
+    posixPermissionToInteger.foreach { entry =>
+      if(permissionSet.contains(entry._1)) {
+        number += entry._2
+      }
+    }
+    number
+  }
+
+  private def getPosixPermissions(unixPermission: Integer): Set[PosixFilePermission] = {
+    val permissions = Set[PosixFilePermission]()
+    posixPermissionToInteger.foreach { entry =>
+      if((unixPermission & entry._2) > 0) {
+        permissions += entry._1
+      }
+    }
+    permissions
+  }
+
+  private def zipDir(rootDir: File, sourceDir: File, out: ZipArchiveOutputStream): Unit = {
     for (file <- sourceDir.listFiles()) {
       if (file.isDirectory()) {
         zipDir(rootDir, new File(sourceDir, file.getName()), out)
       } else {
         var in: FileInputStream = null
         try {
-          val entry = new ZipEntry(file.getPath.substring(rootDir.getPath.length + 1))
-          out.putNextEntry(entry)
+          val entry = new ZipArchiveEntry(file.getPath.substring(rootDir.getPath.length + 1))
+          if(isPosix) {
+            entry.setUnixMode(getUnixPermissionCode(Files.getPosixFilePermissions(Paths.get(file.getPath)).asScala))
+          }
+          out.putArchiveEntry(entry)
           in = new FileInputStream(file)
           IOUtils.copy(in, out)
+          out.closeArchiveEntry()
         }
         finally {
           IOUtils.closeQuietly(in)
@@ -82,6 +127,7 @@ object Utils {
 
   /**
    * Unzip a file to the given directory
+   *
    * @param file file to be unzipped
    * @param targetDir target directory
    */
@@ -92,7 +138,7 @@ object Utils {
 
     val zipFile = new ZipFile(file)
     try {
-      val entries = zipFile.entries()
+      val entries = zipFile.getEntries
       while (entries.hasMoreElements()) {
         val entry = entries.nextElement()
         val targetFile = new File(targetDir, entry.getName)
@@ -113,6 +159,9 @@ object Utils {
             IOUtils.copy(input, output)
             IOUtils.closeQuietly(input)
             IOUtils.closeQuietly(output)
+            if(isPosix) {
+              Files.setPosixFilePermissions(Paths.get(targetFile.getPath), getPosixPermissions(entry.getUnixMode).asJava)
+            }
           }
         }
       }
