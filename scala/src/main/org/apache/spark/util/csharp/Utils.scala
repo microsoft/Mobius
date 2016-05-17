b81f79f3fe25020453e85104b21410f8b7df3e7a
kai <kaizeng@microsoft.com>
2016-05-17 09:56:00 -0700

    Use Apache commons compress library to zip&unzip Mobius application & condensed utils testsuites

--- scala/src/main/org/apache/spark/util/csharp/Utils.scala
+++ scala/src/main/org/apache/spark/util/csharp/Utils.scala
@@ -5,112 +5,105 @@
 package org.apache.spark.util.csharp
 
 import java.io._
-import java.util.{TimerTask, Timer}
-import java.util.zip.{ZipEntry, ZipOutputStream, ZipFile}
-import org.apache.commons.io.IOUtils
+import java.nio.file._
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermission._
+import java.util.{Timer, TimerTask}
 
-import scala.collection.mutable.ArrayBuffer
+import org.apache.commons.compress.archivers.zip.{ZipArchiveEntry, ZipArchiveOutputStream, ZipFile}
+import org.apache.commons.io.{FileUtils, IOUtils}
+import org.apache.spark.Logging
+
+import scala.collection.JavaConverters._
+import scala.collection.Set
 
 /**
  * Utility methods used by SparkCLR.
  */
-object Utils {
+object Utils extends Logging {
+  private val posixFilePermissions = Array(
+    OWNER_READ, OWNER_WRITE, OWNER_EXECUTE,
+    GROUP_READ, GROUP_WRITE, GROUP_EXECUTE,
+    OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE
+  )
 
-  /**
-   * List all entries of a zip file
-   * @param file zip file
-   */
-  def listZipFileEntries(file: File): Seq[String] = {
-
-    val result = ArrayBuffer[String]()
-    val zipFile = new ZipFile(file)
-
-    try {
-      val entries = zipFile.entries()
-      while (entries.hasMoreElements()) {
-        result.append(entries.nextElement().getName)
-      }
-    } finally {
-      zipFile.close()
-    }
-
-    result
-  }
+  val supportPosix = FileSystems.getDefault.supportedFileAttributeViews().contains("posix")
 
   /**
    * Compress all files under given directory into one zip file and drop it to the target directory
-   * @param sourceDir the directory where the zip file will be created
-   * @param targetZipFile
+   *
+   * @param sourceDir source directory to zip
+   * @param targetZipFile target zip file
    */
   def zip(sourceDir: File, targetZipFile: File): Unit = {
-    if (!sourceDir.exists() || !sourceDir.isDirectory) {
-      return
-    }
-
     var fos: FileOutputStream = null
-    var zos: ZipOutputStream = null
+    var zos: ZipArchiveOutputStream = null
     try {
       fos = new FileOutputStream(targetZipFile)
-      zos = new ZipOutputStream(fos)
-      zipDir(sourceDir, sourceDir, zos)
-    } catch {
-      case e: Exception => throw e
+      zos = new ZipArchiveOutputStream(fos)
+
+      val sourcePath = sourceDir.toPath
+      FileUtils.listFiles(sourceDir, null, true).asScala.foreach { file =>
+        var in: FileInputStream = null
+        try {
+          val path = file.toPath
+          val entry = new ZipArchiveEntry(sourcePath.relativize(path).toString)
+          if (supportPosix) {
+            entry.setUnixMode(permissionsToMode(Files.getPosixFilePermissions(path).asScala))
+          }
+          zos.putArchiveEntry(entry)
+
+          in = new FileInputStream(file)
+          IOUtils.copy(in, zos)
+          zos.closeArchiveEntry()
+        } finally {
+          IOUtils.closeQuietly(in)
+        }
+      }
     } finally {
       IOUtils.closeQuietly(zos)
       IOUtils.closeQuietly(fos)
     }
   }
 
-  private def zipDir(rootDir: File, sourceDir: File, out: ZipOutputStream): Unit = {
-    for (file <- sourceDir.listFiles()) {
-      if (file.isDirectory()) {
-        zipDir(rootDir, new File(sourceDir, file.getName()), out)
-      } else {
-        var in: FileInputStream = null
-        try {
-          val entry = new ZipEntry(file.getPath.substring(rootDir.getPath.length + 1))
-          out.putNextEntry(entry)
-          in = new FileInputStream(file)
-          IOUtils.copy(in, out)
-        }
-        finally {
-          IOUtils.closeQuietly(in)
-        }
-      }
-    }
-  }
-
   /**
    * Unzip a file to the given directory
+   *
    * @param file file to be unzipped
    * @param targetDir target directory
    */
   def unzip(file: File, targetDir: File): Unit = {
-    if (!targetDir.exists()){
-      targetDir.mkdir()
-    }
-
-    val zipFile = new ZipFile(file)
+    var zipFile: ZipFile = null
     try {
-      val entries = zipFile.entries()
-      while (entries.hasMoreElements()) {
-        val entry = entries.nextElement()
+      targetDir.mkdirs()
+      zipFile = new ZipFile(file)
+      zipFile.getEntries.asScala.foreach { entry =>
         val targetFile = new File(targetDir, entry.getName)
-        if (!targetFile.getParentFile.exists()) {
-          targetFile.getParentFile.mkdir()
-        }
-        if (entry.isDirectory) {
-          targetFile.mkdirs()
+
+        if (targetFile.exists()) {
+          logWarning(s"Target file/directory $targetFile already exists. Skip it for now. " +
+            s"Make sure this is expected.")
         } else {
-          val input = zipFile.getInputStream(entry)
-          val output = new FileOutputStream(targetFile)
-          IOUtils.copy(input, output)
-          IOUtils.closeQuietly(input)
-          IOUtils.closeQuietly(output)
+          if (entry.isDirectory) {
+            targetFile.mkdirs()
+          } else {
+            targetFile.getParentFile.mkdirs()
+            val input = zipFile.getInputStream(entry)
+            val output = new FileOutputStream(targetFile)
+            IOUtils.copy(input, output)
+            IOUtils.closeQuietly(input)
+            IOUtils.closeQuietly(output)
+            if(supportPosix) {
+              Files.setPosixFilePermissions(
+                targetFile.toPath, modeToPermissions(entry.getUnixMode).asJava)
+            }
+          }
         }
       }
+    } catch {
+      case e: Exception => logError("exception caught during decompression:" + e)
     } finally {
-      zipFile.close()
+      ZipFile.closeQuietly(zipFile)
     }
   }
 
@@ -122,9 +115,7 @@ object Utils {
    */
   def exit(status: Int, maxDelayMillis: Long) {
     try {
-      // scalastyle:off println
-      println(s"Utils.exit() with status: $status, maxDelayMillis: $maxDelayMillis")
-      // scalastyle:on println
+      logInfo(s"Utils.exit() with status: $status, maxDelayMillis: $maxDelayMillis")
 
       // setup a timer, so if nice exit fails, the nasty exit happens
       val timer = new Timer()
@@ -154,4 +145,25 @@ object Utils {
     exit(status, 1000)
   }
 
+  private[spark] def listZipFileEntries(file: File): Array[String] = {
+    var zipFile: ZipFile = null
+    try {
+      zipFile = new ZipFile(file)
+      zipFile.getEntries.asScala.map(_.getName).toArray
+    } finally {
+      ZipFile.closeQuietly(zipFile)
+    }
+  }
+
+  private[this] def permissionsToMode(permissions: Set[PosixFilePermission]): Int = {
+    posixFilePermissions.foldLeft(0) { (mode, perm) =>
+      (mode << 1) | (if (permissions.contains(perm)) 1 else 0)
+    }
+  }
+
+  private[this] def modeToPermissions(mode: Int): Set[PosixFilePermission] = {
+    posixFilePermissions.zipWithIndex
+      .filter { case (_, i) => (mode & (256 >>> i)) != 0 }
+      .map(_._1).toSet
+  }
 }
