467581d7b422fa1ef3cdecc9009d22231bd85455
skaarthik <skaarthik@gmail.com>
2016-06-30 15:30:21 -0700

    added check to run OS-specific path tests

--- scala/src/test/scala/org/apache/spark/deploy/csharp/CSharpRunnerSuite.scala
+++ scala/src/test/scala/org/apache/spark/deploy/csharp/CSharpRunnerSuite.scala
@@ -4,19 +4,24 @@
  */
 package org.apache.spark.deploy.csharp
 
+import org.apache.commons.lang3.SystemUtils
 import org.apache.spark.csharp.SparkCLRFunSuite
 
 class CSharpRunnerSuite extends SparkCLRFunSuite {
   test("formatPath") {
-    // no change to absolute Windows path
-    val path1 = """c:\path\to\Mobius\application.exe"""
-    assert(path1.equals(CSharpRunner.formatPath(path1)))
+    if (SystemUtils.IS_OS_WINDOWS) {
+      // no change to absolute Windows path
+      val path1 =
+        """c:\path\to\Mobius\application.exe"""
+      assert(path1.equals(CSharpRunner.formatPath(path1)))
+    } else {
+      // no change to absolute Linux path
+      val path2 =
+        """/path/to/Mobius/application.sh.exe"""
+      assert(path2.equals(CSharpRunner.formatPath(path2)))
+    }
 
-    // no change to absolute Linux path
-    val path2 = """/path/to/Mobius/application.sh.exe"""
-    assert(path2.equals(CSharpRunner.formatPath(path2)))
-
-    // non-absolute, single-part name is formatted
+    // non-absolute, single-part name is formatted in Windows and Linux
     val path3 = """application.sh.exe"""
     assert(CSharpRunner.formatPath(path3).startsWith("."))
     assert(CSharpRunner.formatPath(path3).endsWith(path3))
