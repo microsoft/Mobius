d3fcc137940a5d61e1b7fab2a6ce066d56825f9c
qualiu <qualiu@microsoft.com>
2016-08-10 23:03:29 +0800

    Update 3 Clean.cmd files to be more safe and convenient to use.

--- cpp/Clean.cmd
+++ cpp/Clean.cmd
@@ -1,4 +1,6 @@
 @ECHO OFF
+pushd %~dp0
 FOR /D /R . %%G IN (bin) DO @IF EXIST "%%G" (@echo RDMR /S /Q "%%G" & rd /s /q "%%G")
 FOR /D /R . %%G IN (obj) DO @IF EXIST "%%G" (@echo RDMR /S /Q "%%G" & rd /s /q "%%G")
 FOR /D /R . %%G IN (x64) DO @IF EXIST "%%G" (@echo RDMR /S /Q "%%G" & rd /s /q "%%G")
+popd
