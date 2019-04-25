517489ab3bc310be7ee807ad5fc51f6cbc0b01ef
ikalyvas <ikalyvas@cite.gr>
2019-04-25 19:37:51 +0300

    Adds serialization helpers for linq expressions

---
+++ csharp/ExpressionSerializer/Data/LinqExpressionData.cs
@@ -0,0 +1,31 @@
+﻿using ExpressionSerializer.ComplexSerializer;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SerializationHelpers.Data
+{
+    public class LinqExpressionData
+    {
+        private String _expressionData;
+
+        public LinqExpressionData(String expressionData)
+        {
+            this._expressionData = expressionData;
+        }
+
+        public Expression<TDelegate> ToExpression<TDelegate>() where TDelegate : Delegate
+        {
+            var serializer = new Serialize.Linq.Serializers.ExpressionSerializer(new JsonLinqSerializer());
+            return (Expression<TDelegate>)serializer.DeserializeText(_expressionData);
+        }
+
+        public TDelegate ToFunc<TDelegate>() where TDelegate : Delegate
+        {
+            var serializer = new Serialize.Linq.Serializers.ExpressionSerializer(new JsonLinqSerializer());
+            var expression = (Expression<TDelegate>)serializer.DeserializeText(_expressionData);
+            return expression.Compile();
+        }
+    }
+}
---
+++ csharp/ExpressionSerializer/Extensions/ExpressionSerializationExtension.cs
@@ -0,0 +1,19 @@
+﻿using ExpressionSerializer.ComplexSerializer;
+using SerializationHelpers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SerializationHelpers.Extensions
+{
+    public static class ExpressionSerializationExtension
+    {
+        public static LinqExpressionData ToExpressionData(this Expression expression)
+        {
+            var serializer = new Serialize.Linq.Serializers.ExpressionSerializer(new JsonLinqSerializer());
+            var expressionValue = serializer.SerializeText(expression);
+            return new LinqExpressionData(expressionValue);
+        }
+    }
+}
